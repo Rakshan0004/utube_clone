09d0dfecbf6b318342b1b0dca8f23d074693283a
Chintala Rakshan <rakshanch0004@gmail.com>
2024-01-17 22:36:54 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/icons-material';
+
+import Navbar from './components/Navbar';
+import ChannelDetail from './components/ChannelDetail';
+import VedioDetail from './components/VedioDetail';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+    <Box sx={{ backgroundcolor: '#000' }}>
+        <Navbar />
+        <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/vedio/:id" element={<VedioDetail />} />
+            <Route path="/channel/:id" element={<ChannelDetail />} />
+            <Route path="/search/:id" element={<SearchFeed />} />
+        </Routes>
+        </Box>
+    </BrowserRouter>
+};
 
-export default App
+export default App;
