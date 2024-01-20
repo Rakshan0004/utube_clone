11eb7d2ead7626304891ca5548fa6a63b53de2cd
Chintala Rakshan <rakshanch0004@gmail.com>
2024-01-20 22:11:07 +0530

    ,,

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Paper, IconButoon } from '@mui/material';
+import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
@@ -13,19 +13,19 @@ const SearchBar = () => {
                 border: '1px solid #e3e3e3',
                 pl: 2,
                 boxShadow: 'none',
-                mr:
+                mr: { sm: 5 }
             }}
-    >
-        <input
-            className="search-bar"
-            placeholder="Search..."
-            value=""
-            onChange={() => {}}
-            />
-            <IconButoon type="submit" sx={{ p: '10px', color: 'red' }}>
-                <Search />
-            </IconButoon>
-        </Paper>
+        >
+            <input
+                className="search-bar"
+                placeholder="Search..."
+                value=""
+                onChange={() => {}}
+                />
+                <IconButoon type="submit" sx={{ p: '10px', color: 'red' }}>
+                    <Search />
+                </IconButoon>
+            </Paper>
     )
 }
 
