2e6fc164d2a4d03a6a78ae0aef4e56931d95395b
Chintala Rakshan <rakshanch0004@gmail.com>
2024-01-21 22:09:24 +0530

    .

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import React, { Box, Stack, Typography } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { Sidebar } from "./";
 
 const Feed = () => {
   return (
-    <Stack sx= {{ flexDirection: { sx: }}}
-  )
-}
+    <Stack sx={{ flexDirection: { sx: 
+    "column", md: "row" } }}>
+      <Box sx={{ height: { 
+        sx: "auto", md: 
+        "92vh" }, borderRight: "1px solid #3d3d3d", 
+        px: { sx: 0, md: 2 } }}>
+        <Sidebar />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
+        </Typography>
+      </Box>
 
-export default Feed
+      
+    </Stack>
+  );
+};
+
+export default Feed;
