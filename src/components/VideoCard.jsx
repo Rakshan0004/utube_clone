8ef6813ae458fe4222cb1b93d30ba72704890d2d
Chintala Rakshan <rakshanch0004@gmail.com>
2024-01-30 23:04:46 +0530

    typo error

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,7 +3,9 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
 import { demoThumbnailUrl, demoVideoUrl, demoChannelTitle } from '../utils/constants';
+
 const VideoCard = () => {
+    console.log(video);
     return (
     <div>VideoCard</div>
     )
