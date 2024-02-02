5744a79fa54ef0d3a1fcb16df449745bdfcabb1d
Chintala Rakshan <rakshanch0004@gmail.com>
2024-02-02 21:28:54 +0530

    nav

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -23,22 +23,20 @@ const ChannelDetail = () => {
     };
 
     fetchResults();
-  }, [id])
+  }, [id]);
 
   return (
     <Box minHeight="95vh">
       <Box>
         <div style={{
-          height:'500px',
-          background: rgb(2,0,36);
-          background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,121,0) 24%, rgba(0,212,255,1) 100%)',
-          zIndex: 15,
-        }} 
-        />
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }} />
         <ChannelCard channelDetail={channelDetail} marginTop="-100px" />
       </Box>
       <Box p={2} display="flex">
-      <Box sx={{ mr: { sm: '100px' }}} />
+      <Box sx={{ mr: { sm: '100px' } }}/>
         <Videos videos={videos} />
       </Box>
     </Box>
